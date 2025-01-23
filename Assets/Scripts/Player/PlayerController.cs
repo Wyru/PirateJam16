6e7f76a2d9159d60128ee1b02167540dfd2c37d9
JotaVe <jotavexd@gmail.com>
2025-01-23 12:17:10 -0300

    PlayerController
    WASD Movement and Look

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,62 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerController : MonoBehaviour
+{
+    [Header("Movement Settings")]
+    public float moveSpeed = 5f;
+    public float acceleration = 5f;
+    public float deceleration = 5f;
+
+    private Rigidbody rb;
+    private Vector2 moveInput;
+    private Vector3 currentVelocity;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+    }
+
+    void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
+    }
+
+    void FixedUpdate()
+    {
+        MovePlayer();
+        RotatePlayer();
+    }
+
+    void MovePlayer()
+    {
+        Vector3 move = new Vector3(moveInput.x, 0, moveInput.y);
+        Vector3 targetVelocity = move * moveSpeed;
+
+        currentVelocity = Vector3.Lerp(currentVelocity, targetVelocity, acceleration * Time.fixedDeltaTime);
+        rb.linearVelocity = currentVelocity;
+
+        if (move != Vector3.zero)
+        {
+            float angle = Mathf.Atan2(move.x, move.z) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0, angle, 0);
+        }
+        else
+        {
+            currentVelocity = Vector3.Lerp(currentVelocity, Vector3.zero, deceleration * Time.fixedDeltaTime);
+            rb.linearVelocity = currentVelocity;
+        }
+    }
+
+    void RotatePlayer()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue());
+        if (Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity))
+        {
+            Vector3 lookAtPoint = new Vector3(hit.point.x, transform.position.y, hit.point.z);
+            transform.LookAt(lookAtPoint);
+        }
+    }
+}
